09232bc6887028a37336bbfc2133709dc45940a2
ycoll <yascoll@gmail.com>
2020-01-27 15:07:46 +0000

    JS added for when cards match and to shuffle

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,5 +10,27 @@ let showCard = function() {
 
 cards.forEach(card => card.addEventListener('click', showCard))
 
+function cardFlip() {
+    flippedCards.push(this);
+    var len = flippedCards.length;
+    if(len === 2){
+        moveCounter();
+        let isMatch = flippedCards[0].type === flippedCards[1].type;
+        isMatch ? matched() : unmatched();        
+    }
+};
 
+function matched(){
+    openedCards[0].classList.add("match");
+    openedCards[1].classList.add("match");
+    openedCards[0].classList.remove("flip");
+    openedCards[1].classList.remove("flip");
+    openedCards = [];
+}
 
+(function shuffle() {
+   cards.forEach(card => {
+     let ramdomPos = Math.floor(Math.random() *16);
+     card.style.order = ramdomPos;
+   });
+ })();
