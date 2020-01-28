c73641178406c61b5daeb207cf0ae5d6698f5d39
ycoll <yascoll@gmail.com>
2020-01-28 17:12:50 +0000

    JS restarted and redone. Cards flip when clicked, match/unmatch functionality added.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,36 +1,52 @@
-//Game loosly based on tutorial https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript
+//Game based on tutorial https://marina-ferreira.github.io/tutorials/js/memory-game/
 
 let card = document.getElementsByClassName("game-card");
 let cards = [ ... card];
 
+let flippedCard = false;
+let firstCard, secondCard;
+
 let showCard = function() {
-    this.classList.toggle("flip");
-    this.classList.toggle("disabled");
-}
+    this.classList.add("flip");
+    this.classList.toggle("disabled")
 
-cards.forEach(card => card.addEventListener('click', showCard))
-
-function cardFlip() {
-    flippedCards.push(this);
-    var len = flippedCards.length;
-    if(len === 2){
-        moveCounter();
-        let isMatch = flippedCards[0].type === flippedCards[1].type;
-        isMatch ? matched() : unmatched();        
+    if (!flippedCard){
+        flippedCard = true;
+        firstCard= this;
+        return;
     }
-};
+   
+    secondCard = this;
+    flippedCard = false;
 
-function matched(){
-    openedCards[0].classList.add("match");
-    openedCards[1].classList.add("match");
-    openedCards[0].classList.remove("flip");
-    openedCards[1].classList.remove("flip");
-    openedCards = [];
+    checkForMatch();
 }
 
-(function shuffle() {
-   cards.forEach(card => {
-     let ramdomPos = Math.floor(Math.random() *16);
-     card.style.order = ramdomPos;
-   });
- })();
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+
+}
+
+function disableCards(){
+    firstCard.classList.add("match");
+    secondCard.classList.add("match");
+    firstCard.removeEventListener('click', showCard);
+    secondCard.removeEventListener('click', showCard);
+
+}
+
+function unflipCards () {
+     firstCard.classList.add('unmatch')
+     secondCard.classList.add('unmatch')
+      
+    setTimeout(() => {
+     firstCard.classList.remove('unmatch')
+     secondCard.classList.remove('unmatch')
+     firstCard.classList.remove('flip');
+     secondCard.classList.remove('flip');
+   }, 1500);
+}
+cards.forEach(card => card.addEventListener('click', showCard));
+
+
