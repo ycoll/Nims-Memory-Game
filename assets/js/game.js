cb9ba99fdc3808c5dc81e200fdefd0a061dc5fa4
ycoll <yascoll@gmail.com>
2020-02-06 20:51:08 +0000

    Basic timer added. Needs more work.

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,34 +3,76 @@
 let card = document.getElementsByClassName("game-card");
 let cards = [ ... card];
 
+let sec = 0;
+let min = 0;
+let time = document.querySelector("#time")
+let interval; 
+let match = document.getElementsByClassName(".match")
+
+
 let flippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
-
+let isMatch;
+    
+timer(time);
+    
 let showCard = function() {
+     
+    
     if (lockBoard) return;
     if (this === firstCard) return
 
     this.classList.add("flip");
-    
+   
 
     if (!flippedCard){
         flippedCard = true;
         firstCard= this;
         return;
     }
-   
+  
     secondCard = this;
     
-
+   
     checkForMatch();
 }
+
+
+ function timer(time){
+    
+    console.log('Im workiing');
+    let interval= setInterval(function(){
+    sec++
+    time.innerHTML = "Time  "+min+" : "+sec;
+   
+
+    if(sec == 60){
+        min ++
+        sec = 0;
+    }
+    
+    if(min == 60){
+        min = 0;
+        sec = 0;
+    }
+    if(match.length == 16){
+        clearInterval(interval)
+
+    }
+    
+}, 1000);
+}
+ 
+
 
 function checkForMatch() {
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCards() : unflipCards();
 
 }
+
+
 
 function disableCards(){
     firstCard.classList.add("match");
@@ -63,14 +105,19 @@ function resetBoard(){
     [firstCard, secondCard] = [null, null];
 }
 
+
+
 (function shuffle() {
    cards.forEach(card => {
-     let ramdomPos = Math.floor(Math.random() * 12);
+     let ramdomPos = Math.floor(Math.random() * 16);
      card.style.order = ramdomPos;
    });
  })();
 
 
-cards.forEach(card => card.addEventListener('click', showCard));
+cards.forEach(card => card.addEventListener('click', showCard ));
+
+ 
+
 
 
